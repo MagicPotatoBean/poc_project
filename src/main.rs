d8c866b0d4b4a55e0ef7068192fddb3ebed7991e
Zoe Soutter <zoe@soutter.com>
2024-10-15 08:56:05 +0100

    Create new log file if it is missing

--- src/main.rs
+++ src/main.rs
@@ -204,12 +204,12 @@ fn garbage_collector_loop(lifetime: Duration) {
 macro_rules! log {
     () => {
         let current_time: DateTime<Utc> = Utc::now();
-        std::fs::OpenOptions::new().append(true).open("err.log").expect("Failed to open log file").write_all(format!("[{} UTC] {}:{}:{}\n", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!()).as_bytes()).expect("Failed to write to log file");
+        std::fs::OpenOptions::new().append(true).create(true).open("err.log").expect("Failed to open log file").write_all(format!("[{} UTC] {}:{}:{}\n", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!()).as_bytes()).expect("Failed to write to log file");
         println!("[{} UTC] {}:{}:{}", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!());
     };
     ($($arg:tt)*) => {{
         let current_time: chrono::DateTime<chrono::Utc> = chrono::Utc::now();
-        std::fs::OpenOptions::new().append(true).open("err.log").expect("Failed to open log file").write_all(format!("[{} UTC] {}:{}:{}: {}\n", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!(), format!($($arg)*)).as_bytes()).expect("Failed to write to log file");
+        std::fs::OpenOptions::new().append(true).create(true).open("err.log").expect("Failed to open log file").write_all(format!("[{} UTC] {}:{}:{}: {}\n", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!(), format!($($arg)*)).as_bytes()).expect("Failed to write to log file");
         println!("[{} UTC] {}:{}:{}: {}", current_time.format("%Y-%m-%d %H:%M:%S"), file!(), line!(), column!(), format!($($arg)*));
     }};
 }
