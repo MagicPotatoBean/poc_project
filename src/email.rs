574a61190e18c4c5e713a7457756f47cb9287f9e
Zoe Soutter <zoe.soutter.com>
2025-01-07 19:05:22 +0000

    Add log macro

--- src/email.rs
+++ src/email.rs
@@ -1,6 +1,6 @@
 use std::{net::SocketAddr, path::PathBuf};
 
-use crate::{http_request::HttpRequest, ROOT_PATH};
+use crate::{http_request::HttpRequest, log, ROOT_PATH};
 
 pub fn email(mut packet: HttpRequest, address: SocketAddr, name: String) {
     if packet.headers().unwrap().get("Authorization")
