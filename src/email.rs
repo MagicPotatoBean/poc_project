f33c1cfd3601bf1f8d81c2f798281121d1f11dbf
Zoe Soutter <zoe.soutter.com>
2025-01-07 19:17:47 +0000

    Debugging

--- src/email.rs
+++ src/email.rs
@@ -28,6 +28,7 @@ pub fn email(mut packet: HttpRequest, address: SocketAddr, name: String) {
     <body>",
         );
         for inbox in inboxes.flatten() {
+            println!("Inbox path = {}", inbox.path());
             html.push_str(&format!(
                 "<a href=\"./email/{}\">{}<a><br>",
                 inbox
