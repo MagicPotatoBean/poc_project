1d127e298fdd3a58e4cf9faa5b9f8cfb38e9783c
Zoe Soutter <zoe@soutter.com>
2025-01-08 15:05:26 +0000

    Fix broken url

--- src/email.rs
+++ src/email.rs
@@ -9,7 +9,7 @@ pub fn email(mut packet: HttpRequest, address: SocketAddr, name: String) {
         println!("Email requested");
         let addr = PathBuf::from(ROOT_PATH.as_path())
             .join("../smtp-rs/inboxes/")
-            .join(&name[7..]);
+            .join(&name.chars().skip(7).collect::<String>());
         let Ok(inboxes) = std::fs::read_dir(
             addr.canonicalize()
                 .expect(&format!("Non-existent inbox: {}", addr.display())),
